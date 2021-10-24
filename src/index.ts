9441e1d1d14bf61a1c63150b182470d79870010c
HaiMT2 <maitrunghai1996@gmail.com>
2021-10-24 15:32:30 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -17,73 +17,55 @@ email: root@gbaranski.com
 
 import { VueElement, QuizQuestion, QuizInfo } from "./types";
 
-const getQuestionsElement = () => {
-  const questionsElem = document.querySelector(
-    "body > div > div.root-component > div > div > div > div.page-container.in-quiz > div.screen.screen-game > div.transitioner.transitioner-component > div > div > div > div > div > div.options-container > div"
-  );
-  if (!questionsElem)
-    throw new Error("Unable to retreive questions list element");
+// const changeElementOpacity = (elem: HTMLElement) => {
+//   elem.style.opacity = "20%";
+// };
 
-  return questionsElem;
-};
+// const highlightAnswers = (question: QuizQuestion) => {
+//   const questionsElem = getQuestionsElement();
+//   const arr: VueElement[] = Array.prototype.slice.call(questionsElem.children);
 
-const changeElementOpacity = (elem: HTMLElement) => {
-  elem.style.opacity = "20%";
-};
+//   if (Array.isArray(question.structure.answer) && question.structure.answer.length < 1 && question.structure.options) {
+//     const answers = question.structure.options.map((option) => option.text).join(" or ");
+//     alert(answers);
 
-const highlightAnswers = (question: QuizQuestion) => {
-  const questionsElem = getQuestionsElement();
-  const arr: VueElement[] = Array.prototype.slice.call(questionsElem.children);
+//     return;
+//   }
 
-  if (Array.isArray(question.structure.answer) && question.structure.answer.length < 1 && question.structure.options) {
-    const answers = question.structure.options.map((option) => option.text).join(" or ");
-    alert(answers);
-
-    return;
-  }
-
-  arr.filter((e) => {
-    if (Array.isArray(question.structure.answer) && question.structure.answer.length > 0) {
-      return !(question.structure.answer.some((ansID) => e.__vue__.optionData.actualIndex === ansID));
-    } else if(typeof question.structure.answer == "number") {
-      return e.__vue__.optionData.actualIndex !== question.structure.answer
-    } else {
-      console.error("Fail detecting type of question: ", question);
-    }
-  }).forEach(changeElementOpacity);
-}
+//   arr.filter((e) => {
+//     if (Array.isArray(question.structure.answer) && question.structure.answer.length > 0) {
+//       return !(question.structure.answer.some((ansID) => e.__vue__.optionData.actualIndex === ansID));
+//     } else if(typeof question.structure.answer == "number") {
+//       return e.__vue__.optionData.actualIndex !== question.structure.answer
+//     } else {
+//       console.error("Fail detecting type of question: ", question);
+//     }
+//   }).forEach(changeElementOpacity);
+// }
 
 const getQuestionInfo = () => {
   const rootObject = document.getElementsByClassName("well--text--2H_p0 well--transition-active--35hDP")[0].textContent;
-  if (!rootObject) throw new Error("Could not retrieve root object");
 
   return rootObject;
 };
 
-// const getRoomHash = (): string => {
-//   const rootObject = document.querySelector("body > div") as VueElement | null;
-//   if (!rootObject) throw new Error("Could not retrieve root object");
-//   const vue = rootObject.__vue__;
-
-//   return vue.$store._vm._data.$$state.game.data.roomHash;
-// }
 
 const msg = `%c 
     Script created by gbaranski#5119! 
     https://github.com/gbaranski/quizizz-cheat
       `;
 
+var sub = "";
 
 (async () => {
   console.log(msg, "color: red;");
 
-  // const quiz: QuizInfo = await (await fetch(`https://quizizz.com/api/main/game/${getRoomHash()}`)).json();
-
-  // let lastQuestionID: string | undefined = undefined;
-
   setInterval(() => {
     const questionInfo = getQuestionInfo();
-    console.log(questionInfo)
+    if (questionInfo && sub != questionInfo)
+    {
+      console.log(questionInfo)
+    }
     // if (questionInfo.questionID !== lastQuestionID) {
     //   for (const q of quiz.data.questions) {
     //     if (questionInfo.questionID === q._id) {
